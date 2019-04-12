3248cd07e2fbf14795070c1cb2706f06c0c21272
jenisonleo <jenisongracious@gmail.com>
2019-04-13 00:33:11 +0530

    inital push

--- hello-world-server.js
+++ hello-world-server.js
@@ -10,7 +10,7 @@ app.use(exp.json());
 app.use('/login',login);
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://jenisonleo:simonblack@ahaaevents-dolod.mongodb.net/events?retryWrites=true";
+const uri = "mongodb+srv://jenisonleo:<passoword>@ahaaevents-dolod.mongodb.net/events?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   // perform actions on the collection object
