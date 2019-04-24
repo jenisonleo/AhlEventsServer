a911ebad31cba2aeb23e176850eb7c999e5b64b2
easwarmoorthy <easwarvem@gmail.com>
2019-04-25 01:00:17 +0530

    Adding put and delete apis

--- routes/api/event.js
+++ routes/api/event.js
@@ -4,7 +4,7 @@ const eventModel = require('../../models/eventSchema.js')
 const mongoose = require('mongoose');
 const {getToken, isUser, isAdmin} = require('../auth.js')
 
-router.post('/event', isAdmin, async function(req, res, next) {
+router.post('/event', isAdmin, async function(req, res) {
   const event = {
     title: req.body.title,
     description: req.body.description,
@@ -16,9 +16,27 @@ router.post('/event', isAdmin, async function(req, res, next) {
   res.send({message:'event added successfully'})
 });
 
-router.get('/events', isUser, async function(req, res, next) {
+router.get('/events', isUser, async function(req, res) {
   const events = await eventModel.find({});
   res.send(events);
+})
+
+router.put('/event/:id', isAdmin, async function(req, res) {
+  await eventModel.updateOne({_id: req.params.id},{
+    $set:{
+      title: req.body.title,
+      description: req.body.description,
+      place: req.body.place,
+      fromDate: req.body.fromDate,
+      toDate: req.body.toDate
+    }
+  });
+  res.send({message: 'Event updated'});
+})
+
+router.delete('/event/:id', isAdmin, async function(req, res) {
+  await eventModel.findByIdAndRemove(req.params.id)
+  res.send({message:'Event deleted'})
 })
 
 module.exports = router;
