a6286349f9ace455e02749ac128c65788901efcc
easwarmoorthy <easwarvem@gmail.com>
2019-05-16 00:33:43 +0530

    added birthdate and phone number fields to user

--- routes/auth.js
+++ routes/auth.js
@@ -2,41 +2,37 @@ const userModel = require('../models/userSchema.js')
 const jwt = require('jsonwebtoken')
 const SECRET = 'Secret8'
 
-
 const getToken = async function (user) {
-  const token = await jwt.sign({ _id: user._id},SECRET).toString()
-  await userModel.updateOne({_id:user._id}, {$set:{ token}})
+  const token = await jwt.sign({ _id: user._id }, SECRET).toString()
+  await userModel.updateOne({ _id: user._id }, { $set: { token } })
   return token
 }
 
-const isUser = async function(req, res, next) {
+const isUser = async function (req, res, next) {
   const token = req.header('Authorization')
-  if(token) {
-    const user = await userModel.findOne({token},{username:1,email:1})
-    if(user) {
+  if (token) {
+    const user = await userModel.findOne({ token }, { username: 1, email: 1 })
+    if (user) {
       req.user = user
       next()
+    } else {
+      res.send({ message: 'User not authorized, invalid token' })
     }
-    else {
-      res.send({message: "User not authorized, invalid token"})
-    }
-  }
-  else {
-    res.send({message: "User not authorized, invalid token"})
+  } else {
+    res.send({ message: 'User not authorized, invalid token' })
   }
 }
 
-const isAdmin = async function(req, res, next) {
+const isAdmin = async function (req, res, next) {
   const token = req.header('Authorization')
-  if(token) {
-    const user = await userModel.findOne({token},{admin: true})
-    if(user) {
+  if (token) {
+    const user = await userModel.findOne({ token }, { admin: true })
+    if (user) {
       next()
-    }
-    else {
-      res.send({message: "User is not admin or invalid token"})
+    } else {
+      res.send({ message: 'User is not admin or invalid token' })
     }
   }
 }
 
-module.exports = {getToken, isUser, isAdmin}
+module.exports = { getToken, isUser, isAdmin }
